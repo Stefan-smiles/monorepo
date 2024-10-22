e48d0f101b7fba8f32ba4518e9558d9f58e6fe4d
Stefan-smiles <s.cookesec@gmail.com>
2024-10-22 09:20:46 +0100

    oh no repo

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(8080, function () {
+  console.log(`Server is running on port 8080`);
+});
